171d802a5e10f9700581029f832346edb061958e
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2021-03-11 16:05:22 -0300

    (-) warning

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState } from 'react';
-import {  Link  } from 'react-router-dom';
 import ImgLogin from "../../imgs/imgLogin.jpg"
 import './stylesLogin.css'
 import elipse1 from '../../imgs/Ellipse1.svg'
